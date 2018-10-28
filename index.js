8b77c0c28f9b208722b0c757b9151844d946c77a
Luka Mis <luka.mis@gmail.com>
2018-10-28 14:55:57 +0100

    Loading basic html in browser window.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+// declare it globally so it will not be garbage collected
+let mainWindow
 
 app.on('ready', () => {
   console.log('app is ready')
+
+  const htmlPath = path.join('src', 'index.html')
+
+  mainWindow = new BrowserWindow()
+
+  mainWindow.loadFile(htmlPath)
 })
